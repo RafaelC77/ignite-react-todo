5fcfdac0c1f74415dacb9222292d5bfdcc94931b
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-07-18 10:51:55 -0300

    feat: create add new task funcionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
+import { useEffect, useState } from "react";
+
 import { Header } from "./components/Header";
 import { NewTaskBar } from "./components/NewTaskBar";
+import { TaskList } from "./components/TaskList";
 
 import "./global.css";
 import styles from "./App.module.css";
-import { TaskList } from "./components/TaskList";
+
+interface Task {
+  id: string;
+  description: string;
+  isComplete: boolean;
+}
 
 export function App() {
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  console.log(taskList);
+
   return (
     <div>
       <Header />
 
       <main className={styles.wrapper}>
-        <NewTaskBar />
+        <NewTaskBar taskList={taskList} onCreateNewTask={setTaskList} />
 
         <TaskList />
       </main>
--- src/components/NewTaskBar.tsx
+++ src/components/NewTaskBar.tsx
@@ -1,14 +1,49 @@
-import { FiPlusCircle } from "react-icons/fi";
+import { FormEvent, useState } from "react";
+import { uid } from "uid";
 
 import styles from "./NewTaskBar.module.css";
 
-export function NewTaskBar() {
+interface Task {
+  id: string;
+  description: string;
+  isComplete: boolean;
+}
+
+interface NewTaskBarProps {
+  taskList: Task[];
+  onCreateNewTask: (taskList: Task[]) => void;
+}
+
+export function NewTaskBar({ taskList, onCreateNewTask }: NewTaskBarProps) {
+  const [taskDescription, setTaskDescription] = useState("");
+
+  function handleCreateNewtask(event: FormEvent) {
+    event.preventDefault();
+
+    const task = {
+      id: uid(),
+      description: taskDescription,
+      isComplete: false,
+    };
+
+    const newTaskList = [...taskList];
+    newTaskList.push(task);
+
+    onCreateNewTask(newTaskList);
+    setTaskDescription("");
+  }
+
   return (
-    <form className={styles.newTaskForm}>
-      <textarea placeholder="Adicione uma nova tarefa" required></textarea>
+    <form className={styles.newTaskForm} onSubmit={handleCreateNewtask}>
+      <textarea
+        onChange={(e) => setTaskDescription(e.target.value)}
+        placeholder="Adicione uma nova tarefa"
+        value={taskDescription}
+        required
+      ></textarea>
       <button type="submit">
         <span>Criar</span>
-        <img src="src/assets/plusIcon.svg" alt="" />
+        <img src="src/assets/plusIcon.svg" />
       </button>
     </form>
   );
